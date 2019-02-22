22a148b39518fcba53faeb92c103048f40256b0f
4byte <medhigh.medhigh@gmail.com>
2019-02-22 19:17:48 +0200

    Initial commit

---
+++ src/main/java/com/example/latest/docker/javadockerexample/JavaDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.latest.docker.javadockerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDockerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/latest/docker/javadockerexample/JavaDockerExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.latest.docker.javadockerexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaDockerExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
